9eebc45a7b879fbeadb1da8525c519fe61509b67
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-02-22 04:34:22 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,34 @@
+let name = "yash"
+
+// console.log(typeof name);
+// console.log(typeof(name));
+
+let num = Number(name);
+// console.log(num); 
+// console.log(typeof num);
+
+//"33" => 33
+//null => 0
+//undefined => NaN
+//"yash" => NaN
+//true => 1; false => 0
+
+let someNumber = 43
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber); 
+// console.log(typeof stringNumber);
+
+let isLoggedIn = 55
+
+let isLoggodInBoolen = Boolean(isLoggedIn)
+// console.log(isLoggodInBoolen); 
+// console.log(typeof isLoggodInBoolen)
+
+//1 => true ; 0 => false
+//null => false
+//undefined => false
+//"" => false; "yash" => true
+
+//--------------- Operations ----------//
+
