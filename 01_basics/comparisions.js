e00c603b04857240d6225fa5cab33902d3438026
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-03-01 03:19:52 +0000

    comparision

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("2" == 1);
+// console.log("02" > 1);
+
+//avoid using these type of comparisions in your code.
+console.log(null > 0); 
+console.log(null < 0); 
+console.log(null == 0); 
+console.log(null >= 0);
+
+console.log(undefined == 0); 
+console.log(undefined > 0); 
+console.log( undefined < 0); 
+console.log(undefined >= 0);
+
+//------------ strict comparision ----------//
+// === > which will compare the data as well as the datatype 
+
+console.log("2" === 2);
+console.log(2 === 2);
