fd8c1d399c55aaf5db134349b056964c1b1f64fb
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-03-01 03:33:46 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -32,3 +32,44 @@ let isLoggodInBoolen = Boolean(isLoggedIn)
 
 //--------------- Operations ----------//
 
+let value = 3;
+let negativeValue = -value;
+// console.log(negativeValue);
+
+// console.log(2 + 3);
+// console.log(2 - 3);
+// console.log(2 * 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " yash"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log(2 + 2 + "1");
+// console.log(2 + 2 + "1" + 1 + "1");
+// console.log("1" + 2 + 2);
+
+// console.log(3 + 4 % (2+5));
+
+// console.log(true);
+// console.log(+true);
+// console.log(+""); 
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2+2
+
+// console.log(num1);
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
