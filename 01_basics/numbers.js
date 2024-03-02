2015860d1b445ea6427fd706b91682fc659bfd14
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-03-02 13:26:21 +0000

    numbers

---
+++ 01_basics/numbers.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100) 
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length); 
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8955
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString());
